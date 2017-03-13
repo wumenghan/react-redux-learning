cb9e582503cc26376818c7fd6875f79c96ae46cc
wumenghan <wu784@purdue.edu>
2017-03-13 09:46:33 -0400

    Leacture 11: writing a todo list reducer

--- index.js
+++ index.js
@@ -133,6 +133,47 @@ const testToggleTodo = () => {
 		).toEqual(todoAfter);
 };
 
+const todos = (state = [], action) => {
+	switch (action.type) {
+		case 'ADD_TODO':
+			return [
+				...state,
+				{
+					id: action.id,
+					text: action.text,
+					completed: false
+
+				}	
+			];
+		default:
+			return state;
+	}
+};
+
+const testAddTodo = () => {
+
+	const stateBefore = [];
+	const action = {
+		type: 'ADD_TODO',
+		id: 0,
+		text: 'Learn Redux'	
+	};
+	const stateAfter = [
+		{
+			id: 0,
+			text: 'Learn Redux',
+			completed: false
+		}
+	];
+
+	deepFreeze(stateBefore);
+	deepFreeze(action);
+	expect(
+		todos(stateBefore, action)
+		).toEqual(stateAfter);
+};
+
+testAddTodo();
 testAddCounter();
 testRemoveCounter();
 testIncrementCounter();
@@ -140,4 +181,3 @@ testToggleTodo();
 console.log('All tests passed.');
 
 
-// lesson 10
